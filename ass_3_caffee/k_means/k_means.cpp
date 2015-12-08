89cf5b6901b474c50c93d4ec2606e7b26312acfc
Linara Adilova <adilova@cuda11.informatik.uni-bonn.de>
2015-12-08 17:09:53 +0100

    assignment 3

--- ass_3_caffee/k_means/k_means.cpp
+++ ass_3_caffee/k_means/k_means.cpp
@@ -87,68 +87,78 @@ int main (int argc, char** argv)
     // square the train vectors
     Blob<Dtype>* blob_train_out = new Blob<Dtype>(1, 1, rows, cols);
     int train_count = train_data->count();
-    caffe_gpu_powx<Dtype>(train_count, train_data->gpu_data(), 2, blob_train_out->mutable_gpu_data());
-    // square the test vectors
-    Blob<Dtype>* blob_test_out = new Blob<Dtype>(1, 1, rows2, cols2);
-    int test_count = test_data->count();
-    caffe_gpu_powx<Dtype>(test_count, test_data->gpu_data(), 2, blob_test_out->mutable_gpu_data());
-    // make 1 matrix, mxn
-    Blob<Dtype>* unary_blob = new Blob<Dtype>(1, 1, rows, rows2);
-    FillerParameter filler_param;
-    filler_param.set_min(1);
-    filler_param.set_max(1);
-    UniformFiller<Dtype> filler(filler_param);
-    filler.Fill(unary_blob);
-    // gather results
-    Blob<Dtype>* result_blob = new Blob<Dtype>(1, 1, rows, rows2);
-    caffe_gpu_gemm<Dtype>(CUBLAS_OP_N, CUBLAS_OP_N, rows, rows2, rows, 1, blob_train_out->gpu_data(), unary_blob->gpu_data(), 0, result_blob->mutable_gpu_data());
-    Blob<Dtype>* temp_blob = new Blob<Dtype>(1, 1, rows, rows2);
-    caffe_gpu_gemm<Dtype>(CUBLAS_OP_N, CUBLAS_OP_N, rows, rows2, rows2, 1, unary_blob->gpu_data(), blob_test_out->gpu_data(), 0, temp_blob->mutable_gpu_data());
-    // keep the sum of them in the first blob
-    int result_count = unary_blob->count();
-    caffe_gpu_add<Dtype>(result_count, result_blob->gpu_data(), temp_blob->gpu_data(), result_blob->mutable_gpu_data());
-    // find scaled multiplication of X and Z
-    caffe_gpu_gemm<Dtype>(CUBLAS_OP_N, CUBLAS_OP_T, rows, rows2, cols, 2, train_data->gpu_data(), test_data->gpu_data(), 0, temp_blob->mutable_gpu_data());
-    // find final D
-    caffe_gpu_sub<Dtype>(result_count, result_blob->gpu_data(), temp_blob->gpu_data(), result_blob->mutable_gpu_data());
+    caffe_gpu_gemm<Dtype>(CblasNoTrans, CblasTrans, rows, rows, cols, 1, train_data->cpu_data(), train_data->cpu_data(), 0,
+                          blob_train_out->mutable_cpu_data());
 
-    // determine the closest for each of the test data
-    Dtype minimal_dist;
-    int minimal_index;
-    Dtype current_dist;
-    ofstream test_classes;
-    test_classes.open("/home/stud/adilova/cuda_vision/ass_3_datasets/test_classes1.txt");
-    for(int c = 0; c < rows2 ; c++){
-      cout << "Test vector number " << c << "--------------\n";
-      minimal_dist = result_blob->mutable_cpu_data()[result_blob->offset(0, 0, 0, c)];
-      minimal_index = 0;
-      for(int r = 0; r < rows ; r++){
-        current_dist = result_blob->mutable_cpu_data()[result_blob->offset(0, 0, r, c)];
-        if(current_dist < minimal_dist) {
-          minimal_dist = current_dist;
-          minimal_index = r;
-        }
+
+   for(int r = 0; r < rows ; r++){
+     for(int c = 0; c < cols ; c++){
+        cout << "from " << train_data->cpu_data()[train_data->offset(0, 0, r, c)];
+        cout << " got " << blob_train_out->cpu_data()[blob_train_out->offset(0, 0, r, c)] << "\n";
       }
-      cout << "minimal distance is " << minimal_dist << " to train vector number " << minimal_index << "\n";
-      count << "class of the vectors is " << classes[minimal_index] << "\n";
-      test_classes << classes[minimal_index] << "\n";
     }
-    test_classes.close();
 
-    for (int i = 0; i < rows; ++i)
-        delete [] train[i];
-    delete [] train;
-    delete [] classes;
-    for (int i = 0; i < rows2; ++i)
-        delete [] test[i];
-    delete [] test;
-    delete train_data;
-    delete test_data;
-    delete blob_train_out;
-    delete blob_test_out;
-    delete unary_blob;
-    delete result_blob;
-    delete temp_blob;
+//    // square the test vectors
+//    Blob<Dtype>* blob_test_out = new Blob<Dtype>(1, 1, rows2, cols2);
+//    int test_count = test_data->count();
+//    caffe_gpu_powx<Dtype>(test_count, test_data->gpu_data(), 2, blob_test_out->mutable_gpu_data());
+//    // make 1 matrix, mxn
+//    Blob<Dtype>* unary_blob = new Blob<Dtype>(1, 1, rows, rows2);
+//    FillerParameter filler_param;
+//    filler_param.set_min(1);
+//    filler_param.set_max(1);
+//    UniformFiller<Dtype> filler(filler_param);
+//    filler.Fill(unary_blob);
+//    // gather results
+//    Blob<Dtype>* result_blob = new Blob<Dtype>(1, 1, rows, rows2);
+//    caffe_gpu_gemm<Dtype>(CblasNoTrans, CblasNoTrans, rows, rows2, rows, 1., blob_train_out->gpu_data(), unary_blob->gpu_data(), 0., result_blob->mutable_gpu_data());
+//    Blob<Dtype>* temp_blob = new Blob<Dtype>(1, 1, rows, rows2);
+//    caffe_gpu_gemm<Dtype>(CblasNoTrans, CblasNoTrans, rows, rows2, rows2, 1, unary_blob->gpu_data(), blob_test_out->gpu_data(), 0, temp_blob->mutable_gpu_data());
+//    // keep the sum of them in the first blob
+//    int result_count = unary_blob->count();
+//    caffe_gpu_add<Dtype>(result_count, result_blob->gpu_data(), temp_blob->gpu_data(), result_blob->mutable_gpu_data());
+//    // find scaled multiplication of X and Z
+//    caffe_gpu_gemm<Dtype>(CblasNoTrans, CblasNoTrans, rows, rows2, cols, 2, train_data->gpu_data(), test_data->gpu_data(), 0, temp_blob->mutable_gpu_data());
+//    // find final D
+//    caffe_gpu_sub<Dtype>(result_count, result_blob->gpu_data(), temp_blob->gpu_data(), result_blob->mutable_gpu_data());
+
+//    // determine the closest for each of the test data
+//    Dtype minimal_dist;
+//    int minimal_index;
+//    Dtype current_dist;
+//    ofstream test_classes;
+//    test_classes.open("/home/stud/adilova/cuda_vision/ass_3_datasets/test_classes1.txt");
+//    for(int c = 0; c < rows2 ; c++){
+//      cout << "Test vector number " << c << "--------------\n";
+//      minimal_dist = result_blob->mutable_cpu_data()[result_blob->offset(0, 0, 0, c)];
+//      minimal_index = 0;
+//      for(int r = 0; r < rows ; r++){
+//        current_dist = result_blob->mutable_cpu_data()[result_blob->offset(0, 0, r, c)];
+//        if(current_dist < minimal_dist) {
+//          minimal_dist = current_dist;
+//          minimal_index = r;
+//        }
+//      }
+//      cout << "minimal distance is " << minimal_dist << " to train vector number " << minimal_index << "\n";
+//      cout << "class of the vectors is " << classes[minimal_index] << "\n";
+//      test_classes << classes[minimal_index] << "\n";
+//    }
+//    test_classes.close();
+
+//    for (int i = 0; i < rows; ++i)
+//        delete [] train[i];
+//    delete [] train;
+//    delete [] classes;
+//    for (int i = 0; i < rows2; ++i)
+//        delete [] test[i];
+//    delete [] test;
+//    delete train_data;
+//    delete test_data;
+//    delete blob_train_out;
+//    delete blob_test_out;
+//    delete unary_blob;
+//    delete result_blob;
+//    delete temp_blob;
 
     return 0;
 }
