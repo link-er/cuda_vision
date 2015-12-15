6eb3dc63d8add9f91f89ce26aced8e9d4083b6c8
Linara Adilova <adilova@cuda11.informatik.uni-bonn.de>
2015-12-15 11:51:48 +0100

    result

--- ass_3_caffee/k_means/build/CMakeFiles/Makefile.cmake
+++ ass_3_caffee/k_means/build/CMakeFiles/Makefile.cmake
@@ -43,6 +43,5 @@ SET(CMAKE_MAKEFILE_PRODUCTS
 
 # Dependency information for all targets:
 SET(CMAKE_DEPEND_INFO_FILES
-  "CMakeFiles/test_blob.dir/DependInfo.cmake"
-  "CMakeFiles/test_math.dir/DependInfo.cmake"
+  "CMakeFiles/k_means.dir/DependInfo.cmake"
   )
