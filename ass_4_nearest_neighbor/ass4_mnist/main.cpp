098121766badd27b55b621e3942bdc814931f592
Linara Adilova <adilova@bigcuda3.informatik.uni-bonn.de>
2015-12-22 17:27:13 +0100

    mnist k-neighbors

--- ass_4_nearest_neighbor/ass4_mnist/main.cpp
+++ ass_4_nearest_neighbor/ass4_mnist/main.cpp
@@ -28,12 +28,20 @@ int main(int argc, char** argv) {
 
     int n = 60000;
     int m = 10000;
+    int d = 28*28;
+
+    data.blob_train_images->Reshape(1, 1, n, d);
+    data.blob_test_images->Reshape(1, 1, m, d);
+
+    cout << "Reshaped\n";
 
     Blob<Dtype>* blob_train_out = new Blob<Dtype>(1, 1, n, d);
-    caffe_gpu_powx<Dtype>(blob_train_out->count(), train_data->gpu_data(), 2.0, blob_train_out->mutable_gpu_data());
+    caffe_gpu_powx<Dtype>(data.blob_train_images->count(), data.blob_train_images->gpu_data(), 2.0, blob_train_out->mutable_gpu_data());
 
     Blob<Dtype>* blob_test_out = new Blob<Dtype>(1, 1, m, d);
-    caffe_gpu_powx<Dtype>(test_data->count(), test_data->gpu_data(), 2.0, blob_test_out->mutable_gpu_data());
+    caffe_gpu_powx<Dtype>(data.blob_test_images->count(), data.blob_test_images->gpu_data(), 2.0, blob_test_out->mutable_gpu_data());
+
+    cout << "Squared\n";
 
     Blob<Dtype>* unary_blob1 = new Blob<Dtype>(1, 1, d, m);
     FillerParameter filler_param;
@@ -45,25 +53,26 @@ int main(int argc, char** argv) {
     Blob<Dtype>* result_blob = new Blob<Dtype>(1, 1, n, m);
     caffe_gpu_gemm<Dtype>(CblasNoTrans, CblasNoTrans, n, m, d, 1., blob_train_out->gpu_data(),
                           unary_blob1->gpu_data(), 0., result_blob->mutable_gpu_data());
+    delete unary_blob1;
+    delete blob_train_out;
 
     Blob<Dtype>* unary_blob2 = new Blob<Dtype>(1, 1, n, d);
     filler.Fill(unary_blob2);
 
-    Blob<Dtype>* temp_blob = new Blob<Dtype>(1, 1, n, m);
     caffe_gpu_gemm<Dtype>(CblasNoTrans, CblasTrans, n, m, d, 1, unary_blob2->gpu_data(),
-                           blob_test_out->gpu_data(), 0, temp_blob->mutable_gpu_data());
+                           blob_test_out->gpu_data(), 1, result_blob->mutable_gpu_data());
+    delete unary_blob2;
+    delete blob_test_out;
+    cout << "Multiplied by unary and added the first one multiplied\n";
 
-    caffe_gpu_add<Dtype>(result_blob->count(), result_blob->gpu_data(), temp_blob->gpu_data(), result_blob->mutable_gpu_data());
+    caffe_gpu_gemm<Dtype>(CblasNoTrans, CblasTrans, n, m, d, -2., data.blob_train_images->gpu_data(),
+                          data.blob_test_images->gpu_data(), 1., result_blob->mutable_gpu_data());
+    cout << "Got final distances\n";
 
-    caffe_gpu_gemm<Dtype>(CblasNoTrans, CblasTrans, n, m, d, 2., train_data->gpu_data(),
-                          test_data->gpu_data(), 0., temp_blob->mutable_gpu_data());
+    Dtype minimal_dist, current_dist;
+    int minimal_index, errors = 0;
 
-    caffe_gpu_sub<Dtype>(result_blob->count(), result_blob->gpu_data(), temp_blob->gpu_data(), result_blob->mutable_gpu_data());
-
-    Dtype minimal_dist;
-    int minimal_index;
-    Dtype current_dist;
-    int errors = 0;
+    cout << "Processing the results\n";
     for(int c = 0; c < m ; c++){
       minimal_dist = result_blob->mutable_cpu_data()[result_blob->offset(0, 0, 0, c)];
       minimal_index = 0;
@@ -74,12 +83,19 @@ int main(int argc, char** argv) {
           minimal_index = r;
         }
       }
-//      classes[minimal_index];
+      int label = data.blob_train_labels->cpu_data()[data.blob_train_labels->offset(minimal_index,0,0,0)];
+      cout << ".";
 
-//      if(current_class != classes[minimal_index])
-//          errors++;
+      if(label != data.blob_test_labels->cpu_data()[data.blob_test_labels->offset(c,0,0,0)])
+          errors++;
     }
-    cout << "Error rate is " << errors*1.0/m << "\n";
+    cout << "\nError rate is " << errors*1.0/m << "\n";
+
+    delete result_blob;
+    delete data.blob_train_images;
+    delete data.blob_test_images;
+    delete data.blob_train_labels;
+    delete data.blob_test_labels;
 
     return 0;
 }
