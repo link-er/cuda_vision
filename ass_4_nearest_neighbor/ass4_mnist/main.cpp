7ab93f4dc4028e8e3b5962f9700431786a1dc192
link_er <link_er@bk.ru>
2016-01-04 18:14:22 +0100

    corrected version of argmax layer

--- ass_4_nearest_neighbor/ass4_mnist/main.cpp
+++ ass_4_nearest_neighbor/ass4_mnist/main.cpp
@@ -71,58 +71,58 @@ int main(int argc, char** argv) {
     delete data.blob_train_images;
     delete data.blob_test_images;
 
-    // // working with argmax layer
-    // cout << "Preparing layer\n";
-    // Blob<Dtype>* blob_top = new Blob<Dtype>();
-    // vector<Blob<Dtype>*> blob_bottom_vec;
-    // vector<Blob<Dtype>*> blob_top_vec;
+    // working with argmax layer
+    cout << "Preparing layer\n";
+    Blob<Dtype>* blob_top = new Blob<Dtype>();
+    vector<Blob<Dtype>*> blob_bottom_vec;
+    vector<Blob<Dtype>*> blob_top_vec;
 
-    // caffe_gpu_scale<Dtype>(n*m, -1, result_blob->gpu_data(), result_blob->mutable_gpu_data());
-    // result_blob->Reshape(n, m, 1, 1);
-    // blob_bottom_vec.push_back(result_blob);
-    // blob_top_vec.push_back(blob_top);
+    caffe_gpu_scale<Dtype>(m*n, -1, result_blob->gpu_data(), result_blob->mutable_gpu_data());
+    result_blob->Reshape(m, n, 1, 1);
+    blob_bottom_vec.push_back(result_blob);
+    blob_top_vec.push_back(blob_top);
 
-    // LayerParameter layer_param;
-    // ArgMaxParameter* argmax_param = layer_param.mutable_argmax_param();
-    // argmax_param->set_out_max_val(false);
-    // ArgMaxLayer<Dtype> layer(layer_param);
-    // layer.SetUp(blob_bottom_vec, blob_top_vec);
-    // cout << "Getting forward step results\n";
-    // layer.Forward(blob_bottom_vec, blob_top_vec);
+    LayerParameter layer_param;
+    ArgMaxParameter* argmax_param = layer_param.mutable_argmax_param();
+    argmax_param->set_out_max_val(false);
+    ArgMaxLayer<Dtype> layer(layer_param);
+    layer.SetUp(blob_bottom_vec, blob_top_vec);
+    cout << "Getting forward step results\n";
+    layer.Forward(blob_bottom_vec, blob_top_vec);
 
-    // cout << "Counting error\n";
-    // int label, max_index, errors = 0;
-    // const Dtype* top_data = blob_top->cpu_data();
-    // for(int c = 0; c < m ; c++){
-    //   max_index = top_data[blob_top->offset(c,0,0,0)];
-    //   label = data.blob_train_labels->cpu_data()[data.blob_train_labels->offset(max_index,0,0,0)];
-    //   if(label != data.blob_test_labels->cpu_data()[data.blob_test_labels->offset(c,0,0,0)])
-    //     errors++;
-    // }
-    // cout << "\nError rate is " << errors*1.0/m << "\n";
-
-    Dtype minimal_dist, current_dist;
-    int minimal_index, label, errors = 0;
-
-    cout << "Processing the results\n";
+    cout << "Counting error\n";
+    int label, max_index, errors = 0;
+    const Dtype* top_data = blob_top->cpu_data();
     for(int c = 0; c < m ; c++){
-      minimal_dist = result_blob->mutable_cpu_data()[result_blob->offset(0, 0, c, 0)];
-      minimal_index = 0;
-      for(int r = 0; r < n ; r++){
-        current_dist = result_blob->mutable_cpu_data()[result_blob->offset(0, 0, c, r)];
-        if(current_dist < minimal_dist) {
-          minimal_dist = current_dist;
-          minimal_index = r;
-        }
-      }
-      label = data.blob_train_labels->cpu_data()[data.blob_train_labels->offset(minimal_index,0,0,0)];
-      if(c%100 == 0)
-        cout << ".";
-
+      max_index = top_data[blob_top->offset(c,0,0,0)];
+      label = data.blob_train_labels->cpu_data()[data.blob_train_labels->offset(max_index,0,0,0)];
       if(label != data.blob_test_labels->cpu_data()[data.blob_test_labels->offset(c,0,0,0)])
-          errors++;
+        errors++;
     }
     cout << "\nError rate is " << errors*1.0/m << "\n";
+
+    // Dtype minimal_dist, current_dist;
+    // int minimal_index, label, errors = 0;
+
+    // cout << "Processing the results\n";
+    // for(int c = 0; c < m ; c++){
+    //   minimal_dist = result_blob->mutable_cpu_data()[result_blob->offset(0, 0, c, 0)];
+    //   minimal_index = 0;
+    //   for(int r = 0; r < n ; r++){
+    //     current_dist = result_blob->mutable_cpu_data()[result_blob->offset(0, 0, c, r)];
+    //     if(current_dist < minimal_dist) {
+    //       minimal_dist = current_dist;
+    //       minimal_index = r;
+    //     }
+    //   }
+    //   label = data.blob_train_labels->cpu_data()[data.blob_train_labels->offset(minimal_index,0,0,0)];
+    //   if(c%100 == 0)
+    //     cout << ".";
+
+    //   if(label != data.blob_test_labels->cpu_data()[data.blob_test_labels->offset(c,0,0,0)])
+    //       errors++;
+    // }
+    // cout << "\nError rate is " << errors*1.0/m << "\n";
 
     delete result_blob;
     delete data.blob_train_labels;
