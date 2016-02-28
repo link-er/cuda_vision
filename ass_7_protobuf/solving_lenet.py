6899ec21c331233baa5c060a34fca66eb900e4ec
Linara Adilova <adilova@bigcuda3.informatik.uni-bonn.de>
2016-02-28 23:28:03 +0100

    displaying lenet

--- ass_7_protobuf/solving_lenet.py
+++ ass_7_protobuf/solving_lenet.py
@@ -2,6 +2,8 @@ import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 
+import numpy as np
+
 caffe_root = '/home/stud/adilova/caffe/caffe-rc3/'
 
 import sys
@@ -47,10 +49,10 @@ solver = None  # ignore this workaround for lmdb data (can't instantiate two sol
 solver = caffe.SGDSolver('mnist/lenet_auto_solver.prototxt')
 
 # each output is (batch size, feature dim, spatial dim)
-[(k, v.data.shape) for k, v in solver.net.blobs.items()]
+print [(k, v.data.shape) for k, v in solver.net.blobs.items()]
 
 # just print the weight sizes (we'll omit the biases)
-[(k, v[0].data.shape) for k, v in solver.net.params.items()]
+print [(k, v[0].data.shape) for k, v in solver.net.params.items()]
 
 solver.net.forward()  # train net
 solver.test_nets[0].forward()  # test net (there can be more than one)
@@ -63,4 +65,61 @@ plt.savefig('/home/stud/adilova/cuda_vision/ass_7_protobuf/train.pdf')
 plt.imshow(solver.test_nets[0].blobs['data'].data[:8, 0].transpose(1, 0, 2).reshape(28, 8*28), cmap='gray'); plt.axis('off')
 print 'test labels:', solver.test_nets[0].blobs['label'].data[:8]
 plt.savefig('/home/stud/adilova/cuda_vision/ass_7_protobuf/test.pdf')
+
+solver.step(1)
+plt.imshow(solver.net.params['conv1'][0].diff[:, 0].reshape(4, 5, 5, 5).transpose(0, 2, 1, 3).reshape(4*5, 5*5), cmap='gray'); plt.axis('off')
+plt.savefig('/home/stud/adilova/cuda_vision/ass_7_protobuf/conv1.pdf')
+
+#time
+niter = 200
+test_interval = 25
+# losses will also be stored in the log
+train_loss = np.zeros(niter)
+test_acc = np.zeros(int(np.ceil(niter / test_interval)))
+output = np.zeros((niter, 8, 10))
+
+# the main solver loop
+for it in range(niter):
+    solver.step(1)  # SGD by Caffe
+    
+    # store the train loss
+    train_loss[it] = solver.net.blobs['loss'].data
+    
+    # store the output on the first test batch
+    # (start the forward pass at conv1 to avoid loading new data)
+    solver.test_nets[0].forward(start='conv1')
+    output[it] = solver.test_nets[0].blobs['score'].data[:8]
+    
+    # run a full test every so often
+    # (Caffe can also do this for us and write to a log, but we show here
+    #  how to do it directly in Python, where more complicated things are easier.)
+    if it % test_interval == 0:
+        print 'Iteration', it, 'testing...'
+        correct = 0
+        for test_it in range(100):
+            solver.test_nets[0].forward()
+            correct += sum(solver.test_nets[0].blobs['score'].data.argmax(1)
+                           == solver.test_nets[0].blobs['label'].data)
+        test_acc[it // test_interval] = correct / 1e4
+
+_, ax1 = plt.subplots()
+ax2 = ax1.twinx()
+ax1.plot(np.arange(niter), train_loss)
+ax2.plot(test_interval * np.arange(len(test_acc)), test_acc, 'r')
+ax1.set_xlabel('iteration')
+ax1.set_ylabel('train loss')
+ax2.set_ylabel('test accuracy')
+ax2.set_title('Test Accuracy: {:.2f}'.format(test_acc[-1]))
+plt.savefig('/home/stud/adilova/cuda_vision/ass_7_protobuf/test_accuracy.pdf')
+
+for i in range(8):
+    plt.figure(figsize=(2, 2))
+    plt.imshow(solver.test_nets[0].blobs['data'].data[i, 0], cmap='gray')
+    plt.savefig('/home/stud/adilova/cuda_vision/ass_7_protobuf/original' + str(i) + '.pdf')
+    plt.figure(figsize=(10, 2))
+    plt.imshow(output[:50, i].T, interpolation='nearest', cmap='gray')
+    plt.xlabel('iteration')
+    plt.ylabel('label')
+    plt.savefig('/home/stud/adilova/cuda_vision/ass_7_protobuf/training' + str(i) + '.pdf')
+
 
