b74651f8ab5f4612cc59cc8dcd16f02d9d173038
Linara Adilova <adilova@bigcuda3.informatik.uni-bonn.de>
2016-03-09 23:15:57 +0100

    assignment 8 flickr trained finetune

--- ass_8_finetuning/flickr_tuning.py
+++ ass_8_finetuning/flickr_tuning.py
@@ -3,6 +3,8 @@ import sys
 sys.path.insert(0, path_caffe + 'python')
 
 import numpy as np
+import matplotlib
+matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 import lmdb
 from pylab import *
@@ -28,4 +30,25 @@ for it in range(niter):
     scratch_train_loss[it] = scratch_solver.net.blobs['loss'].data
     if it % 10 == 0:
         print 'iter %d, finetune_loss=%f, scratch_loss=%f' % (it, train_loss[it], scratch_train_loss[it])
+
+fig = plt.figure()
+ax1 = fig.add_subplot(1,2,1)
+ax1.plot(np.vstack([train_loss, scratch_train_loss]).T)
+ax2 = fig.add_subplot(1,2,2)
+ax2.plot(np.vstack([train_loss, scratch_train_loss]).clip(0, 4).T)
+plt.savefig('/home/VI/stud/adilova/cuda_vision/ass_8_finetuning/training.png')
+
+test_iters = 10
+accuracy = 0
+scratch_accuracy = 0
+for it in arange(test_iters):
+    solver.test_nets[0].forward()
+    accuracy += solver.test_nets[0].blobs['accuracy'].data
+    scratch_solver.test_nets[0].forward()
+    scratch_accuracy += scratch_solver.test_nets[0].blobs['accuracy'].data
+accuracy /= test_iters
+scratch_accuracy /= test_iters
+print 'Accuracy for fine-tuning:', accuracy
+print 'Accuracy for training from scratch:', scratch_accuracy
+
 
